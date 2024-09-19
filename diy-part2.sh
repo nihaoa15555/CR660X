752413fce6e91db70db8cafd056c77bce4221e17
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-09-19 20:48:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,5 +23,3 @@ git clone https://github.com/CHN-beta/rkp-ipid.git package/rkp-ipid
 git clone https://github.com/nihaoa15555/luci-app-ua2f.git package/luci-app-ua2f
 git clone https://github.com/nihaoa15555/luci-app-nettask.git package/luci-app-nettest
 git clone https://github.com/nihaoa15555/mac1.git package/luci-mac
-git clone https://github.com/koshev-msk/modemfeed package/luci-ess
-git clone https://github.com/nihaoa15555/luci-app-eqosplus package/luci-eqos
