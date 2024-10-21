ca2509b93dd3b2739703e0bc7ebe82f1e1e26de4
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-10-21 22:25:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,8 +18,8 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='HuaWei'' packag
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 #sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/OpenWrt /某不知名人类/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/Xiaomi Mi Router CR660X/g" target/linux/ramips/dts/mt7621_xiaomi_mi-router-cr6606.dts
+sed -i "s/OpenWrt /某不知名人类制作/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/HuaiWei Mate XT 非凡大师/g" target/linux/ramips/dts/mt7621_xiaomi_mi-router-cr6606.dts
 
 #删除原默认主题
 rm -rf package/lean/luci-theme-argon
